67574324cd84fdf56a318dc3ef90c38e1fa6d825
Kobi Keren <kobikms728@outlook.com>
2016-11-02 10:14:23 +0200

    started the project

---
+++ Game_X_O_SLN/Game_X_O/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_X_O
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+
+            Console.ReadLine();
+        }
+    }
+}
